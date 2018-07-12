b3e8fb218b46fb1b88935007800ded770111f361
christinarivas <christina@rivastwins.com>
2018-07-12 17:40:07 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 class Shoes
  # attr_accessor :author, :page_count, :genre
-  attr_reader :shoes
+  #attr_reader :shoes
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
  
   
