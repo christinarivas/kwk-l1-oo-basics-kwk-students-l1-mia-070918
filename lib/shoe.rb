358fb31302012afa75ff32c53c692d5807c7f9af
christinarivas <christina@rivastwins.com>
2018-07-12 17:47:57 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,15 +4,21 @@ class Shoes
  
   def initialize(brand, color)
     @brand = brand
+    @color = color
   end
   
   def brand
     @brand
   end
- 
+ def color
+   @color
+ end
   
  
 end
 
 shoe1=Shoes.new("adidas")
 puts shoe1.brand
+
+color1=Shoes.new("red")
+puts color1
